1d0b870d7738858fc0550c37ecf76dc4201702c0
MauricioAB <dev.mauricioalezandre@gmail.com>
2020-08-23 13:16:01 -0300

    Exercices.

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -7,36 +7,31 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.get("/products", function (req, res) {
-  res.json(products);
-});
-
-app.post("/products", function (req, res) {
-  const newProduct = { ...req.body, id: products.length + 1 };
-  products = [...products, newProduct];
-  res.json(newProduct);
-});
-
-app.put("/products", function (req, res) {
-  let updatedProduct;
-  products = products.map((p) => {
-    if (p.id === req.body.id) {
-      updatedProduct = { ...p, ...req.body };
-      return updatedProduct;
-    }
-    return p;
+app
+  .route("/products")
+  .get((req, res) => {
+    res.json(products);
+  })
+  .post((req, res) => {
+    const newProduct = { ...req.body, id: products.length + 1 };
+    products = [...products, newProduct];
+    res.json(newProduct);
+  })
+  .put((req, res) => {
+    let updatedProduct;
+    products = products.map((p) => {
+      if (p.id === req.body.id) {
+        updatedProduct = { ...p, ...req.body };
+        return updatedProduct;
+      }
+      return p;
+    });
+    res.json(updatedProduct);
+  })
+  .delete((req, res) => {
+    const deletedProduct = products.find((p) => p.id === +req.body.id);
+    products = products.filter((p) => p.id !== +req.body.id);
+    res.json(deletedProduct);
   });
-  res.json(updatedProduct);
-});
-
-app.delete("/products/:id", function (req, res) {
-  const deletedProduct = products.find((p) => p.id === +req.params.id);
-  products = products.filter((p) => p.id !== +req.params.id);
-  res.json(deletedProduct);
-});
-
-app.get("/products", (req, res) => {
-  // implement
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
